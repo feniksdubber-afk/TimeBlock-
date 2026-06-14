eeaef9037d5306164f682f56a6e0f9549ea37de6
feniksdubber-afk <feniksdubber@gmail.com>
2026-06-14 15:49:48 +0500

    Add files via upload

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,45 @@
+export enum Category {
+  Ish = "Ish",
+  Sport = "Sport",
+  Oqish = "Oqish",
+  DamOlish = "DamOlish",
+  Ovqat = "Ovqat",
+  Oila = "Oila",
+  Muhim = "Muhim",
+  Shaxsiy = "Shaxsiy",
+  Boshqa = "Boshqa",
+}
 
+export enum RepeatType {
+  HarKuni = "HarKuni",
+  HarHafta = "HarHafta",
+  IshKunlari = "IshKunlari",
+  DamOlishKunlari = "DamOlishKunlari",
+  HarOy = "HarOy",
+  HarYil = "HarYil",
+}
+
+export interface BlockShape {
+  cols: number;
+  rows: number;
+}
+
+export interface Block {
+  id: string;
+  emoji: string;
+  name: string;
+  category: Category;
+  color: string;
+  durationSlots: number;
+  startSlot: number;
+  shape: BlockShape;
+  repeat: RepeatType;
+}
+
+export interface TimeSlot {
+  index: number;
+  hour: number;
+  minute: number;
+  isOccupied: boolean;
+  blockId?: string;
+}
